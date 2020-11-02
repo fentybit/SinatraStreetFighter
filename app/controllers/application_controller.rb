c952b4d24b5f9cb1b6a2e27cc19475f4775e67ca
fentyhall <fentymhall@gmail.com>
2020-11-02 10:50:00 -0800

    update configure sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions 
-    set :session_secret, "secret"
+    set :session_secret, ENV["SESSION_SECRET"]
   end
 
   get "/" do
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,8 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+require 'dotenv/load'
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
