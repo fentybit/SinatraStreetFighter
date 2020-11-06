9f1713057a3d469aa7a04c79928a5e6d320e0d03
fentyhall <fentymhall@gmail.com>
2020-11-05 21:04:48 -0800

    update seeds database

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,6 +30,36 @@ characters_list = {
         :bio => "A former air force pilot and Guile's mentor, after being killed by M Bison he is resurrected by Kolin with the sole purpose of eliminating M Bison and Shadaloo.",
         :image => "/images/nash.jpg",
         :video => "https://www.youtube.com/embed/lXbJq14tCdI?start=27"
+    },
+    "Abigail" => {
+        :quote => "You can't drive anywhere without POWER! Vrooooom!",
+        :bio => "First appearing in Final Fight as one of the bosses of the Mad Gear Gang, Abigail has left his life of crime behind to run a small garage in Metro City named 'Abigail's Scrap Metal' as a law abiding citizen.",
+        :image => "/images/abigail.jpg",
+        :video => "https://www.youtube.com/embed/xGTMxVTF_Xg?start=36"
+    },
+    "Cammy" => {
+        :quote => "Being someone who has gone through so many scenes of carnage, those kind of cheap attacks won't reach me!",
+        :bio => "A former Bison Doll, she was saved by Dhalsim and later joined Delta Red to help defeat Shadaloo and save the other dolls like her.",
+        :image => "/images/cammy.jpg",
+        :video => "https://www.youtube.com/embed/Jk0yfpNtvww?start=13"
+    },
+    "Sakura" => {
+        :quote => "I'll be a great fighter one day, just you wait and see!",
+        :bio => "Sakura began to participate in street fighting after watching Ryu win the first World Warrior tournament.",
+        :image => "/images/sakura.jpg",
+        :video => "https://www.youtube.com/embed/CSsAYwMhfGo?start=30"
+    },
+    "Vega" => {
+        :quote => "Kukuku... The beautiful do not know defeat.",
+        :bio => "Vega was born to a privileged noble family in Spain. The incident warped his mind, and he developed a dual personality: honorable nobleman by day, sadistic murderer by night.",
+        :image => "/images/vega.jpg",
+        :video => "https://www.youtube.com/embed/7CpA_FnjaRw?start=35"
+    },
+    "Laura" => {
+        :quote => "Boom! That's how you start the fight and finish it quickly!",
+        :bio => "Hailing from Brazil and sister to Sean, Laura seeks to promote her fighting Matsuda Jiu-Jitsu style and grow her Matsuda dojo. She travels the world seeking strong fighters to train with and join her dojo.",
+        :image => "/images/laura.jpg",
+        :video => "https://www.youtube.com/embed/lvVf-vnJ9gc?start=31"
     }
 } 
 
