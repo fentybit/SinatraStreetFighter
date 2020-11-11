4d57c7a43df95ad4df720ad7418fafdf534f43fb
fentyhall <fentymhall@gmail.com>
2020-11-11 12:23:17 -0800

    refactor stages controller and views

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -2,6 +2,10 @@ class StagesController < ApplicationController
     get '/:slug/stages/new' do 
         if logged_in?
             finding_character_slug
+            if @character.moves.empty?
+                flash[:message] = "Please select your fighting moves beforehand."
+                redirect to "/characters/#{@character.slug}"
+            end 
             erb :'stages/new'
         else  
             redirect to '/login'
@@ -11,18 +15,13 @@ class StagesController < ApplicationController
     post '/:slug/stages/new' do 
         finding_character_slug
 
-        if !@character.moves.empty?
-            @character.moves.collect do |move|
-                @stage = Stage.find_by(name: params[:stage][:name])
-                move.stages << @stage
-            end 
-            @character.save
-    
-            redirect to "/characters/#{@character.slug}/#{@stage.id}"
-        else  
-            flash[:message] = "Please select your fighting moves beforehand."
-            redirect to "/characters/#{@character.slug}"
-        end    
+        @character.moves.collect do |move|
+            @stage = Stage.find_by(name: params[:stage][:name])
+            move.stages << @stage
+        end 
+        @character.save
+
+        redirect to "/characters/#{@character.slug}/#{@stage.id}"
     end 
 
     get '/:slug/stages/edit' do 
